4658369c899525a0fb0195b38d1f17bb55a0b0f3
BranLiang <lby89757@hotmail.com>
2019-05-03 00:18:46 +0800

    Add basic component button

---
+++ src/theme.ts
@@ -0,0 +1,58 @@
+const theme = {
+  bg: {
+    default: "#FFFFFF",
+    reverse: "#16171A",
+    wash: "#FAFAFA",
+    divider: "#F6F7F8",
+    border: "#EBECED",
+    inactive: "#DFE7EF",
+  },
+  brand: {
+    default: "#36B37E",
+    alt: "#57D9A3",
+    wash: "#E8E5FF",
+    border: "#DDD9FF",
+    dark: "#006644",
+  },
+  generic: {
+    default: "#E6ECF7",
+    alt: "#F6FBFC",
+  },
+  space: {
+    default: "#0062D6",
+    alt: "#1CD2F2",
+    wash: "#E5F0FF",
+    border: "#BDD8FF",
+    dark: "#0F015E",
+  },
+  special: {
+    default: "#E58306",
+    alt: "#F1C742",
+    dark: "#7D4A00",
+    wash: "#FFF5E5",
+    border: "#FFE6BF",
+  },
+  success: {
+    default: "#00B88B",
+    alt: "#00D5BD",
+    dark: "#00663C",
+    wash: "#D9FFF2",
+    border: "#9FF5D9",
+  },
+  text: {
+    default: "#24292E",
+    secondary: "#384047",
+    alt: "#67717A",
+    placeholder: "#7C8894",
+    reverse: "#FFFFFF",
+  },
+  warn: {
+    default: "#E22F2F",
+    alt: "#E2197A",
+    dark: "#85000C",
+    wash: "#FFEDF6",
+    border: "#FFCCE5",
+  },
+};
+
+export default theme;
---
+++ src/utils/color.ts
@@ -0,0 +1,38 @@
+export const tint = (hex: string, amount: number) => {
+  let R = parseInt(hex.substring(1, 3), 16);
+  let G = parseInt(hex.substring(3, 5), 16);
+  let B = parseInt(hex.substring(5, 7), 16);
+
+  const getSingle = (n: number) => parseInt(((n * (100 + amount)) / 100).toString(), 10);
+
+  R = getSingle(R);
+  G = getSingle(G);
+  B = getSingle(B);
+
+  R = R < 255 ? R : 255;
+  G = G < 255 ? G : 255;
+  B = B < 255 ? B : 255;
+
+  const getDouble = (n: number) =>
+    n.toString(16).length === 1
+      ? `0${n.toString(16)}`
+      : n.toString(16);
+
+  const RR = getDouble(R);
+  const GG = getDouble(G);
+  const BB = getDouble(B);
+
+  return `#${RR}${GG}${BB}`;
+};
+
+export const hexa = (hex: string, alpha: number) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  if (alpha >= 0) {
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  } else {
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+};
