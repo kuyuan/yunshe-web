cee08f4dc1ec7f52f3f87b3374323393b4a0cd3b
BranLiang <lby89757@hotmail.com>
2019-05-03 14:02:37 +0800

    Add stories for button

---
+++ stories/button.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Button from '../src/components/Button';
+
+storiesOf('Button', module)
+  .add('Default', () => (
+    <Button kind='default'>Default</Button>
+  ))
+  .add('Primary', () => (
+    <Button kind='primary'>Primary</Button>
+  ))
+  .add('Warning', () => (
+    <Button kind='warning'>Warning</Button>
+  ))
+  .add('Outline', () => (
+    <Button kind='outline'>Outline</Button>
+  ))
+  .add('Warn Outline', () => (
+    <Button kind='warn-outline'>Warn Outline</Button>
+  ))
+  .add('Primary Outline', () => (
+    <Button kind='primary-outline'>Primary Outline</Button>
+  ))
+  .add('Light Outline', () => (
+    <Button kind='light-outline'>Light Outline</Button>
+  ))
+  .add('Text', () => (
+    <Button kind='text'>Text Button</Button>
+  ))
